d310114bb2f84140086a90ff0af3df503af9f242
Guo Yang <94389344+JamesGorton@users.noreply.github.com>
2022-07-18 03:40:01 +0100

    Create qLinear.py

---
+++ qtransformer/qLinear.py
@@ -0,0 +1,106 @@
+# Inherit from Function
+from torch.autograd import Function
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+def get_dynamic_scale(x, bits, with_grad=False):
+    """Calculate dynamic scale for quantization from input by taking the
+    maximum absolute value from x and number of bits"""
+    with torch.set_grad_enabled(with_grad):
+        threshold = x.abs().max()
+    return get_scale(bits, threshold)
+
+def get_scale(bits, threshold):
+    """Calculate scale for quantization according to some constant and number of bits"""
+    return calc_max_quant_value(bits) / threshold
+
+def calc_max_quant_value(bits):
+    """Calculate the maximum symmetric quantized value according to number of bits"""
+    return 2 ** (bits - 1) - 1
+
+def quantize(input): # bits = 8
+    """Do linear quantization to input according to a scale and number of bits"""
+    thresh = 127 # calc_max_quant_value(bits) 
+    scale = get_dynamic_scale(input, 8)
+    return input.mul(scale).round().clamp(-thresh, thresh)
+
+class LinearFunction(Function):
+
+    # Note that both forward and backward are @staticmethods
+    @staticmethod
+    # bias is an optional argument
+    def forward(ctx, input, weight, bias=None):
+        input = quantize(input)
+        weight = quantize(weight)
+        if bias is not None:
+            bias = quantize(bias)
+
+        ctx.save_for_backward(input, weight, bias)
+        output = input.mm(weight)
+        if bias is not None:
+            output += bias.unsqueeze(0).expand_as(output)
+        return output
+
+    # This function has only a single output, so it gets only one gradient
+    @staticmethod
+    def backward(ctx, grad_output):
+        # This is a pattern that is very convenient - at the top of backward
+        # unpack saved_tensors and initialize all gradients w.r.t. inputs to
+        # None. Thanks to the fact that additional trailing Nones are
+        # ignored, the return statement is simple even when the function has
+        # optional inputs.
+        input, weight, bias = ctx.saved_tensors
+        grad_input = grad_weight = grad_bias = None
+        grad_output = quantize(grad_output,1,8)
+
+        # These needs_input_grad checks are optional and there only to
+        # improve efficiency. If you want to make your code simpler, you can
+        # skip them. Returning gradients for inputs that don't require it is
+        # not an error.
+        if ctx.needs_input_grad[0]:
+            grad_input = grad_output.mm(weight)
+        if ctx.needs_input_grad[1]:
+            grad_weight = grad_output.t().mm(input)
+        if bias is not None and ctx.needs_input_grad[2]:
+            grad_bias = grad_output.sum(0)
+
+        return quantize(grad_input,1,8), grad_weight, quantize(grad_bias,1,8)
+
+
+class Linear(nn.Module):
+    def __init__(self, input_features, output_features, bias=True):
+        super(Linear, self).__init__()
+        self.input_features = input_features
+        self.output_features = output_features
+
+        # nn.Parameter is a special kind of Tensor, that will get
+        # automatically registered as Module's parameter once it's assigned
+        # as an attribute. Parameters and buffers need to be registered, or
+        # they won't appear in .parameters() (doesn't apply to buffers), and
+        # won't be converted when e.g. .cuda() is called. You can use
+        # .register_buffer() to register buffers.
+        # nn.Parameters require gradients by default.
+        self.weight = nn.Parameter(torch.empty(output_features, input_features))
+        if bias:
+            self.bias = nn.Parameter(torch.empty(output_features))
+        else:
+            # You should always register all possible parameters, but the
+            # optional ones can be None if you want.
+            self.register_parameter('bias', None)
+
+        # Not a very smart way to initialize weights
+        nn.init.uniform_(self.weight, -0.1, 0.1)
+        if self.bias is not None:
+            nn.init.uniform_(self.bias, -0.1, 0.1)
+
+    def forward(self, input):
+        # See the autograd section for explanation of what happens here.
+        return LinearFunction.apply(input, self.weight, self.bias)
+
+    def extra_repr(self):
+        # (Optional)Set the extra information about this module. You can test
+        # it by printing an object of this class.
+        return 'input_features={}, output_features={}, bias={}'.format(
+            self.input_features, self.output_features, self.bias is not None
+        )
