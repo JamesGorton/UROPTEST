ec86424c876c1d8d18256ecfd334e8b2f66c824f
Guo Yang <gy261@cl.cam.ac.uk>
2022-09-13 16:56:55 +0100

    2

--- generator.py
+++ generator.py
@@ -16,7 +16,7 @@ def format_and_save(script, template, **kwargs):
 
     temp = template.format(**kwargs)
 
-    sc = open(script, 'w')
+    sc = open(script, 'w+')
     sc.write(temp)
     sc.close()
 
@@ -37,8 +37,8 @@ slurm_script_template = (
     "#SBATCH {mem_constraint}\n"
     "export PATH=/pkgs/anaconda3/bin:$PATH\n"
     ". /pkgs/anaconda3/bin/activate {virt_env}\n"
-    "module load python/3.8"
-    "module load intel/mkl/2020.2"
+    "module load python/3.8\n"
+    "module load intel/mkl/2020.2\n"
     "echo \"$(uname -a)\"\n"
     "echo \"$(nvidia-smi)\"\n"
     "echo \"$(pwd)\"\n"
@@ -63,7 +63,7 @@ for i in msfp_set:
 
     command = prefix + f"fairseq-train {data} --arch qtransformer --share-decoder-input-output-embed --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 --dropout 0.3 --weight-decay 0.0001 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 4096 --eval-bleu --eval-bleu-detok moses --eval-bleu-remove-bpe --eval-bleu-print-samples --best-checkpoint-metric bleu --maximize-best-checkpoint-metric "
     command += f"--batch-size {batch_size} --save-dir ./checkpoints/msfp_{i[0]}{i[1]}{i[2]}{i[3]} --tensorboard-logdir ./dir/msfp_{i[0]}{i[1]}{i[2]}{i[3]} --max-epoch {max_epoch} --save-interval {save_interval} --quant-scheme msfp --quant-percentile 1 --quant-bitwidth [{i[0]},{i[1]},{i[2]},{i[3]}] --quant-bucketsize 16 "
-    command += f"--eval-bleu-args {{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}} "
+    command += f"--eval-bleu-args {{beam: 5, max_len_a: 1.2, max_len_b: 10}} "
     _temp += command
 
     format_and_save(
